ed86b01b114c5ed3197e9e723e02ed64e6e46d33
Balram1807 <balramgothi9@gmail.com>
2023-08-21 21:33:20 +0530

    forntend part 1

---
+++ carDispensaryFrontend/src/App.jsx
@@ -0,0 +1,19 @@
+import Layout from "./Components/Layout/Layout";
+import Periodic from "./Components/Services/Periodic";
+
+
+
+
+
+
+function App() {
+   return <Layout/>;
+
+  // return <Periodic></Periodic>
+  
+  
+  
+
+}
+
+export default App;
---
+++ carDispensaryFrontend/src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+
+import Periodic from "../Services/Periodic";
+import "../Services/Service"
+import Service from "../Services/Service";
+const Home = ()=>
+{
+    return(
+        
+            <div>
+                <Service/>
+                <Periodic/>
+            </div>
+        
+    )
+}
+export default Home;
